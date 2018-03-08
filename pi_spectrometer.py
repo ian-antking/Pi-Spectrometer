f40b06042c60eb10d3aea6cdd25e40947b8b293a
Ian King <36984875+classimago@users.noreply.github.com>
2018-03-08 20:42:24 +0000

    Update pi_spectrometer.py
    Changes:
    Re-built with Tkinter gui. 
    Keyboard bindings added to root window and camera preview. 
    Instructions added to camera preview.
    
    To do:
    Add menu bar.
    Add camera settings menu.
    Add About window.
    Add Quit function.

--- pi_spectrometer.py
+++ pi_spectrometer.py
@@ -1,10 +1,12 @@
+import tkinter as tk
+from tkinter import messagebox
 import picamera
 import time
-from PIL import Image
-from guizero import *
+from PIL import ImageTk, Image
 import os
 import subprocess
 import io
+from threading import Thread
 
 camera = picamera.PiCamera(framerate = 10)
 
@@ -13,6 +15,9 @@ camera.iso = 1600
 camera.shutter_speed = 6000000000
 camera.zoom= (0.25, 0.25, 0.5, 0.5)
 camera.resolution = (1280,720)
+
+global preview_active
+preview_active = False
 
 spectra_directory = '/home/pi/Pictures/pi_spectrometer'
 upload_directory = '/home/pi/Pictures/pi_spectrometer/processed_spectra'
@@ -23,26 +28,36 @@ if not os.path.exists(spectra_directory):
 if not os.path.exists(upload_directory):
     os.makedirs(upload_directory) 
 
-            
 def open_pictures():
     subprocess.call(['xdg-open', spectra_directory])
 
-    
+def capture_spectrum(event):
+    start_preview()
+
 def start_preview():
-    sample_name = sample_input.value
-    if sample_input.value and sample_input.value.strip():
-
-        crosshair = Image.new('RGB', (2000,1), color='white')
-        camera.start_preview()
-        #camera.annotate_text = '[space] to capture, [esc] to cancel'
     
+    if not sample_input.get():
+        messagebox.showerror('error','You must enter a sample ID')
+        root.focus_set()
+    else:
+        crosshair = Image.new('RGB', (2000,1), color='white')
+        camera.start_preview(alpha = 128)
+        camera.annotate_text = '[esc] to exit, [space] to capture'
+        global o
         o = camera.add_overlay(crosshair.tobytes(), size=(1280,1), layer=3, alpha=128, format='rgba')
+        #camera.preview.alpha = 125
+        global preview_active
+        preview_active = True
+
+
         
-        #camera.preview.alpha = 70
-
-
+def spacebar(event):
+    global preview_active
+    if preview_active:
+        sample_name = sample_input.get()
+        global spectra_directory
+        global upload_directory
         stream=io.BytesIO()
-        time.sleep(30)
         camera.capture(stream, format='png')
         img = Image.open(stream)
         spectrum_path = '%s/%s.png' % (spectra_directory, sample_name)
@@ -56,37 +71,53 @@ def start_preview():
         img2.save(upload_path)
         img2.close()
         img3.close()
-        last_image.value = spectrum_path
-        last_action.value = 'Spectrum saved to: %s' % spectrum_path
+        new_spectrum = ImageTk.PhotoImage(Image.open(spectrum_path))
+        last_image.configure(image = new_spectrum)
+        last_image.image = new_spectrum #keep reference
         camera.stop_preview()
-        camera.remove_overlay(o)  
-        sample_input.clear()
+        camera.remove_overlay(o)
+        status.configure(text = 'Spectrum saved to: %s' % spectrum_path)
+        preview_active=False
         
-       
-    else:
-        warn("error","Please enter a sample name!")
+    
+def esc_key(event):
+    global preview_active
+    if preview_active:
+        camera.stop_preview()
+        camera.remove_overlay(o)
+        preview_active=False
+        
+        
+root = tk.Tk()
+
+last_spectrum = ImageTk.PhotoImage(Image.new('RGB', (1280, 50), color = 'black'))
+
+
+root.title('Pi-Spectrometer')
+root.geometry('1280x200')
+sample_controls = tk.Frame(root)
+
+root.bind('<space>', spacebar)
+root.bind('<Escape>', esc_key)
+root.bind('<Return>', capture_spectrum)
+
+sample_instruction = tk.Label(sample_controls, text='Sample ID:')
+sample_input = tk.Entry(sample_controls)
+take_sample = tk.Button(sample_controls, text='Capture Spectrum', command=start_preview)
+last_image = tk.Label(image=last_spectrum)
+last_image.image = last_spectrum
+view_spectra = tk.Button(root, text='View Spectra', command = open_pictures)
+status = tk.Label(root, text="ready", bd=1, relief=tk.SUNKEN, anchor=tk.W)
+
+status.pack(side=tk.BOTTOM, expand=1, fill=tk.X, pady=0)
+sample_controls.pack(side=tk.TOP, pady=50, padx=10)
+sample_instruction.pack(side=tk.LEFT)
+sample_input.pack(side=tk.LEFT)
+take_sample.pack(side=tk.LEFT)
+view_spectra.pack(side=tk.RIGHT, padx=10, pady=10)
+last_image.pack()
 
 
 
-app = App(title="Pi-Spectrometer", layout="grid", width=1280, height = 155)
 
-welcome = Text(app, text='', grid = [0,0,2,1])
-
-sample_instruction = Text(app, text='Sample ID:', grid=[0,1], align='right')
-
-sample_input = TextBox(app, text="", grid = [1,1], align = 'left',)
-
-sample_input.width = 20
-
-take_image = PushButton(app, command=start_preview, text='Take Reading', grid=[2,1], align='left')
-
-placeholder = Image.new('RGB', (1280, 50), color = 'black')
-placeholder.save('%s/placeholder.png' % spectra_directory)
-last_image = Picture(app, '%s/placeholder.png' % spectra_directory, grid=[0,3,3,1])
-
-
-show_files = PushButton(app, text='Show Files', command = open_pictures, grid=[2,4], align='right')
-last_action = Text(app, text='Please enter a sample ID', align='left', grid=[0,4])
-
-app.display()
-
+root.mainloop()
